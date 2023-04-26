3b66a3deea28a368456892e8c35b15f360e47ec8
Ivan <ivan.yarmoliuk@gmail.com>
2023-04-26 07:38:58 -0700

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Ivan Yarmoliuk on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
